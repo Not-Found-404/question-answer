630bc380af64dc8e0e28282483a37f99d6d7e404
BillowsTao <taobillows@163.com>
2018-08-28 00:03:36 +0800

    完成界面布局和设计

--- js/question-answer.js
+++ js/question-answer.js
@@ -1,4 +1,14 @@
 /* NF4 园丁助手 - 答题界面
  * author: BillowsTao
- * date: 2018.8.27 
+ * date: 2018.8.27
  */
+
+$(function initEventHandle() {
+    /* 绑定事件初始化函数 */
+
+})
+
+/*  */
+$(".option > .option-item").bind("click", function questionCheckClick(event) {
+    alert(event.value);
+});
